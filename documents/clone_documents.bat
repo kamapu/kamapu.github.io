951695137bb8429bb5f72dd1fcb690aa58cf3b70
Miguel Alvarez <kamapu78@gmail.com>
2018-04-24 17:38:46 +0200

    Links to SWEA and IMPETUS projects added

--- documents/clone_documents.bat
+++ documents/clone_documents.bat
@@ -1,3 +1,6 @@
 
+REM Repositories 2018-04-24
+xcopy /y "M:\WorkspaceR\00_Documents\GIS_repositories\GIS_Repositories.pdf" "M:\WorkspaceR\kamapu.github.io\documents\*"
+
 REM Workshop Freiburg 2017-03-09
 xcopy /y "M:\WorkspaceR\vegtable_workshops\Freiburg_2017\workshops_freiburg_2017.html" "M:\WorkspaceR\kamapu.github.io\documents\*"
