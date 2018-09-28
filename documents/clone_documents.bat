8e6fdd7f5ccad0c1016d9a4333e8bd3b7c8dc09f
Miguel Alvarez <kamapu78@gmail.com>
2018-09-28 20:19:48 +0200

    New workshop and additional format of homepage

--- documents/clone_documents.bat
+++
@@ -1,9 +0,0 @@
-
-REM Repositories 2018-04-24
-xcopy /y "M:\WorkspaceR\00_Documents\GIS_repositories\GIS_Repositories.pdf" "M:\WorkspaceR\kamapu.github.io\documents\*"
-
-REM Workshop Freiburg 2017-03-09
-xcopy /y "M:\WorkspaceR\00_Documents\introduction_vegtable\workshops_freiburg_2017.html" "M:\WorkspaceR\kamapu.github.io\documents\*"
-
-REM Publications
-xcopy /y "M:\WorkspaceR\00_Documents\CV_Miguel\Publications_Miguel.pdf" "M:\WorkspaceR\kamapu.github.io\documents\*"
